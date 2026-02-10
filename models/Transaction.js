a9bdbf749c6977bca7c1f1e23baa45067a1c3025
Terenz Jahred Dantes <terenzdantes717@gmail.com>
2026-02-10 23:18:09 +0800

    feat: implement server setup and add Transaction model schema

---
+++ models/Transaction.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const TransactionSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add some text']
+    },
+    amount: {
+        type: Number,
+        required: [true, 'Please add a positive or negative number']
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: false
+    }
+})
+
+export default TransactionSchema;
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+import express from 'express';
+import cors from 'cors'
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-
 connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({msg: 'Budget Tracker Backend is running!'});
+})
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
