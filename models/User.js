7276938a045f4e80024938b84b7b66e0d94756a7
Terenz Jahred Dantes <terenzdantes717@gmail.com>
2026-03-01 15:18:59 +0800

    feat: add User model with password hashing and JWT token generation

---
+++ models/User.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false
+    }
+})
+
+// This function runs automatically RIGHT BEFORE we save a user to the database
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10); // Generates a random 'Salt'
+    this.password = await bcrypt.hash(this.password, salt); // Scramble the password
+})
+
+// This is a helper function to create the "ID Card" (Token)
+
+UserSchema.methods.getSignedJwtToken = function() {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+}
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
