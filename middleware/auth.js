d6cac52c727b047c2009fff1a9d7b6d03fb65601
Terenz Jahred Dantes <terenzdantes717@gmail.com>
2026-03-03 00:20:49 +0800

    fix: correct token extraction and ensure next middleware is called in protect function

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,27 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-export const protect = async (req, res) => {
+export const protect = async (req, res, next) => {
     let token; 
 
     if ( req.headers.authorization && req.headers.authorization.startsWith('Bearer') ) {
-        token = req.headers.authorization.split('')[1];
+        token = req.headers.authorization.split(' ')[1];
     }
 
     if (!token) {
         return res.status(401).json({ success: false, error: 'Not authorized to access this route' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = await User.findById(decoded.id);
+        next();
+    } catch {
+        res.status(401).json({
+            success: false,
+            error: 'Not Authorized!'
+        })
     }
 
     
